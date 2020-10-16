f3a7fc80394ef2fefe16254791a59dc4bc59f8a0
slb <jiang.zhengchao@outlook.com>
2020-10-16 02:18:50 -0400

    init commit!

---
+++ README.md
@@ -0,0 +1,3 @@
+CMakeList.txt for imgui on linux with SDLand2  opengl3(glew).
+
+Tested on OpenSUSE15.02
---
+++ cmake-build-debug/CMakeFiles/uDataShore.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/slb/CLionProjects/uDataShore/imgui/imgui.cpp" "/home/slb/CLionProjects/uDataShore/cmake-build-debug/CMakeFiles/uDataShore.dir/imgui/imgui.cpp.o"
+  "/home/slb/CLionProjects/uDataShore/imgui/imgui_demo.cpp" "/home/slb/CLionProjects/uDataShore/cmake-build-debug/CMakeFiles/uDataShore.dir/imgui/imgui_demo.cpp.o"
+  "/home/slb/CLionProjects/uDataShore/imgui/imgui_draw.cpp" "/home/slb/CLionProjects/uDataShore/cmake-build-debug/CMakeFiles/uDataShore.dir/imgui/imgui_draw.cpp.o"
+  "/home/slb/CLionProjects/uDataShore/imgui/imgui_impl_opengl3.cpp" "/home/slb/CLionProjects/uDataShore/cmake-build-debug/CMakeFiles/uDataShore.dir/imgui/imgui_impl_opengl3.cpp.o"
+  "/home/slb/CLionProjects/uDataShore/imgui/imgui_impl_sdl.cpp" "/home/slb/CLionProjects/uDataShore/cmake-build-debug/CMakeFiles/uDataShore.dir/imgui/imgui_impl_sdl.cpp.o"
+  "/home/slb/CLionProjects/uDataShore/imgui/imgui_widgets.cpp" "/home/slb/CLionProjects/uDataShore/cmake-build-debug/CMakeFiles/uDataShore.dir/imgui/imgui_widgets.cpp.o"
+  "/home/slb/CLionProjects/uDataShore/main.cpp" "/home/slb/CLionProjects/uDataShore/cmake-build-debug/CMakeFiles/uDataShore.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/SDL2"
+  ".././imgui"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
